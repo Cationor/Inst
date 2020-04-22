b61887e69b8c12538cc5544db3ef5848af150189
kiryl_kashuba <kashuba.kirya@gmail.ru>
2020-04-22 22:30:59 +0300

    2

--- back/src/main/java/com/netcracker/backend/service/StudentService.java
+++ back/src/main/java/com/netcracker/backend/service/StudentService.java
@@ -5,7 +5,7 @@ import com.netcracker.backend.entity.Student;
 
 import java.util.List;
 
-public interface StudentService {
+public interface StudentService {//
     Student find (String name);
     List<Student> getAll();
 }
