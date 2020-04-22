ada960a934707c1265af29318fcf4eff40dda778
kiryl_kashuba <kashuba.kirya@gmail.ru>
2020-04-22 23:31:34 +0300

    test

--- back/src/main/java/com/netcracker/backend/controller/StudentController.java
+++ back/src/main/java/com/netcracker/backend/controller/StudentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/student")
+@RequestMapping("api/student") //test
 public class StudentController {
     @Autowired
     private StudentService studentService;
